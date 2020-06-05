01ca96f3f7ccf04602509abb16cb2256a7658253
t190044 <t190044@mail.ryukoku.ac.jp>
2020-06-05 02:12:12 +0000

    提出

---
+++ test1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int n,a=1,i,k,s=1,t;
+    while(1){
+        s=s*2;
+        if(s<0) break;
+        t=s;
+    }
+    while(1){
+        n=1;
+        for(i=1;i<=a;i++){
+            n=n*a;
+        }
+        if(n<t && n>0) printf("%d\n",n);
+        else break;
+        a++;
+    }
+}
